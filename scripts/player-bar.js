051a95d9a4fb548cd5d6d0deb9a994041aee4ea6
Will <willliam213@gmail.com>
2018-12-20 16:11:51 -0800

    assignment 10 complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -4,24 +4,31 @@
       $(this).attr('playState', player.playState);
     });
 
-     
-   $('button#next').on('click', function() {
-     //only works if playstate = playing
+  $('button#next').on('click', function() {
+    //only works if playstate = playing
     if (player.playState !== 'playing') { return; }
-    
     //grabs the value of index of next song
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-     const nextSongIndex = currentSongIndex + 1;
-     
+    const nextSongIndex = currentSongIndex + 1;
     //prevents the button from doing anything if on last song
      if (nextSongIndex >= album.songs.length) { return; }
+    //assigns the next song based on the nextsongIndex to a variable
+    const nextSong = album.songs[nextSongIndex];
+    //call players to play that song
+    player.playPause(nextSong);
+  });
 
-
-     //assigns the next song based on the nextsongIndex to a variable
-     const nextSong = album.songs[nextSongIndex];
-
-     //call players to play that song
-     player.playPause(nextSong);
-    });
-
-  }
+  $('button#previous').on('click', function() {
+    //only works if playstate = playing
+    if (player.playState !== 'playing') { return; }
+    //grabs the value of index of previous song
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const prevSongIndex = currentSongIndex - 1;
+    //prevents the button from doing anything if on first song
+    if (currentSongIndex == 0) { return; }
+    //assigns the next song based on the nextsongIndex to a variable
+    const prevSong = album.songs[prevSongIndex];
+    //call players to play that song
+    player.playPause(prevSong);
+  });
+}
